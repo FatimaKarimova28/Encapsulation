eb668318da79dfbaed166bc4b507ad79789ca396
FatimaKarimova28 <fatimaika@code.edu.az>
2023-01-30 18:28:24 +0400

    done

---
+++ Encapsulation/Invoice.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Encapsulation
+{
+    public class Invoice
+    {
+
+        int account;
+        string customer;
+        string provider;
+        
+
+        public Invoice(int account, string customer, string provider)
+        {
+
+            this.account = account;
+            this.customer = customer;   
+            this.provider = provider;   
+
+
+        }
+
+        private string articl;
+        
+        public string Article
+        {
+            get
+            {
+                return articl;
+            }
+            set
+            {
+                articl = value;
+
+            }
+        }
+
+            private int quantity;
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        private decimal price;
+        public decimal Price { get
+            { 
+                return price;
+            }
+            set 
+            { 
+                price = value;  
+            } }
+        public void CostCalculation(bool needEdv)
+        {
+
+            if (needEdv == true)
+            {
+                Console.WriteLine(Price * Quantity *(decimal) 0.18); 
+
+            }
+            else
+            {
+                Console.WriteLine(Price * Quantity); 
+            }
+
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Encapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var inv = new Invoice(678904, "Alex", "Foxtrot");
+            {
+
+
+                inv.Article = "USB-hab";
+                inv.Quantity = 6;
+                inv.Price = 30;
+
+
+
+                inv.CostCalculation(true);
+            }
+            
+            
+            
+        }
+        
+      
+    }
+}
